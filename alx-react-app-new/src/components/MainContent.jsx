c91d6c3d87e9b1a7ff71b43e894400c52387da91
Idoko Augustine <linkwithaugustine@gmail.com>
2025-07-20 13:09:04 -0700

    updated MainContent.jsx file

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ function MainContent() {
         <main style={{ padding: '1rem', backgroundColor: '#f4f4f4' }}>
             <h2 style={{ textAlign: 'center', color: '#444' }}>Welcome to My Favorite Cities</h2>
             <p style={{ lineHeight: '1.6', fontSize: '1rem' }}>
-                Here you'll find profiles of users and their favorite cities around the world.
+                I love to visit New York, Paris, and Tokyo.
             </p>
       </main>
     );
