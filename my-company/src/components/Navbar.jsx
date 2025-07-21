6d984f7060c8430edd8f7273e964cb3b145024d7
Idoko Augustine <linkwithaugustine@gmail.com>
2025-07-20 19:54:56 -0700

    added display and justifycontent styles to navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#eee' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#eee', display: 'inline-block', justifyContent: 'space-around'}}>
       <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
       <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
       <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
