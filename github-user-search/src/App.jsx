cdb0700d8c09bcdaf8091432ccfbf062066bb61a
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-02 01:14:29 -0700

    initiated Github user search app with React

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+ 
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Search Profiles</h1>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
