e51e5451901563e36bf36082b9abb77fcf4659b3
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-06 23:53:05 -0700

    added responsive design using tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 md:p-8 rounded-lg shadow-lg max-w-xs md:max-w-sm mx-auto my-10">
-        <img className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
-        <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-sm text-gray-600 md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="bg-gray-100 sm:p-4 md:p-8 rounded-lg shadow-lg sm:max-w-xs md:max-w-sm mx-auto my-10">
+        <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <p className="sm:text-sm text-gray-600 md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
