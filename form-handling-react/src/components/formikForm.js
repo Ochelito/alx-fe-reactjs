b2bda8da4ca35ba32dcd6a7182b75e47eca0db4f
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-19 18:05:56 -0700

    implemented form handling with controlled component and formik

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 function FormikForm() {
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      email: "",
-      password: "",
-    },
+  return React.createElement(Formik, {
+    initialValues: { username: "", email: "", password: "" },
     validationSchema: Yup.object({
       username: Yup.string().required("Username is required"),
       email: Yup.string().email("Invalid email").required("Email is required"),
@@ -18,59 +14,35 @@ function FormikForm() {
       console.log("Formik form submitted:", values);
       alert("Registration successful with Formik!");
     },
+    children: (formik) =>
+      React.createElement(
+        "form",
+        { onSubmit: formik.handleSubmit },
+        <>
+          <h2>User Registration (Formik)</h2>
+
+          <div>
+            <label>Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="p" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="p" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="p" style={{ color: "red" }} />
+          </div>
+
+          <button type="submit">Register</button>
+        </>
+      ),
   });
-
-  return React.createElement(
-    "form",
-    { onSubmit: formik.handleSubmit },
-    <>
-      <h2>User Registration (Formik)</h2>
-
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={formik.values.username}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.username && formik.errors.username && (
-          <p style={{ color: "red" }}>{formik.errors.username}</p>
-        )}
-      </div>
-
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <p style={{ color: "red" }}>{formik.errors.email}</p>
-        )}
-      </div>
-
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.password && formik.errors.password && (
-          <p style={{ color: "red" }}>{formik.errors.password}</p>
-        )}
-      </div>
-
-      <button type="submit">Register</button>
-    </>
-  );
 }
 
 export default FormikForm;
